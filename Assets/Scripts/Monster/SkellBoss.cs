3e439c97b43bd6c450921e329851a7cac2f4da97
김범무 <ehwjs0023@gmail.com>
2024-03-12 17:55:03 +0900

    몬스터 상태 패턴 구현

--- Assets/Scripts/Monster/SkellBoss.cs
+++ Assets/Scripts/Monster/SkellBoss.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkellBoss : MonoBehaviour
+public enum BossState { Idle, Attack, Die }
+public class SkellBoss : Monster
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
