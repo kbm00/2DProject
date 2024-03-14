894c5d346c485d22a4a904336530fc5e946fb084
김범무 <ehwjs0023@gmail.com>
2024-03-14 11:26:01 +0900

    feat : 드래그시 이미지 변경

--- Assets/Scripts/Scene/TitleScene.cs
+++ Assets/Scripts/Scene/TitleScene.cs
@@ -1,14 +1,30 @@
 using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class TitleScene : BaseScene
+public class TitleScene : BaseScene, IPointerEnterHandler, IPointerExitHandler
 {
+    public Image buttonImage;
+    public Sprite defaultSprite; 
+    public Sprite dragSprite; 
     public override IEnumerator LoadingRoutine()
     {
         yield return null;
     }
-
+  
     public void StartGame()
     {
         Manager.Scene.LoadScene("GameScene");
+    }
+   
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonImage.sprite = dragSprite;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonImage.sprite = defaultSprite;
     }
 }
