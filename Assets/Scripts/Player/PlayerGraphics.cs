d09bf3a39fc9d57e92583f09eb0d1ed77be84d54
kbm00 <156034787+kbm00@users.noreply.github.com>
2024-03-15 08:44:54 +0900

    feat : 검 전환시 총알 나가는 문제 수정

--- Assets/Scripts/Player/PlayerGraphics.cs
+++ Assets/Scripts/Player/PlayerGraphics.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,11 +9,12 @@ public class PlayerGraphics : MonoBehaviour
     [SerializeField] Transform weaponPoint;
     [SerializeField] Transform firePoint;
     [SerializeField] float bulletSpeed;
+    [SerializeField] Animator animator;
 
     [Header("Prefabs")]
     [SerializeField] GameObject bulletPrefab;
 
-
+    private bool isAttacking = false;
     private int currentWeaponIndex = 0;
 
     void Update()
@@ -78,31 +80,49 @@ public class PlayerGraphics : MonoBehaviour
     {
 
 
-        GameObject projectile = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
-        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
-
-        Vector2 direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - firePoint.position);
-        direction.Normalize();
-
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        // 플레이어가 왼쪽을 바라보고 있을 때
-        if (playerTransform.localScale.x < 0)
+        // 현재 활성화된 무기가 첫 번째 무기일 때만 총알 발사
+        if (currentWeaponIndex == 0)
         {
-            // 발사체의 회전을 조정하여 왼쪽으로
-            projectile.transform.rotation = Quaternion.Euler(0, 0, angle - 180);
-        }
-        else
-        {
-            // 플레이어가 오른쪽을 바라보고 있을 때 발사체의 정상 회전을 유지
-            projectile.transform.rotation = Quaternion.Euler(0, 0, angle);
-        }
+            GameObject projectile = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
 
-        rb.velocity = direction * bulletSpeed;
+            Vector2 direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - firePoint.position);
+            direction.Normalize();
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            // 플레이어가 왼쪽을 바라보고 있을 때
+            if (playerTransform.localScale.x < 0)
+            {
+                // 발사체의 회전을 조정하여 왼쪽으로
+                projectile.transform.rotation = Quaternion.Euler(0, 0, angle - 180);
+            }
+            else
+            {
+                // 플레이어가 오른쪽을 바라보고 있을 때 발사체의 정상 회전을 유지
+                projectile.transform.rotation = Quaternion.Euler(0, 0, angle);
+            }
+
+            rb.velocity = direction * bulletSpeed;
+        }
     }
     private void OnAttack(InputValue value)
     {
-        Fire();
+        Fire(); // 총을 사용하는 경우
+
+        if (currentWeaponIndex == 1 && !isAttacking) // 1번 인덱스인 검일 때만
+        {
+            isAttacking = true;
+            animator.SetBool("isSwing", true);
+            StartCoroutine(ResetSwordAnimation());
+        }
+    }
+
+    IEnumerator ResetSwordAnimation()
+    {
+        yield return new WaitForSeconds(0.5f); // 애니메이션 연속 방지 
+        animator.SetBool("isSwing", false);
+        isAttacking = false;
     }
 
     private void Shoot()
@@ -111,7 +131,7 @@ public class PlayerGraphics : MonoBehaviour
         Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
 
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = 0; // Z축 값은 무시
+        mousePosition.z = 0; // Z축 값 무시
 
         Vector2 fireDirection = mousePosition - firePoint.position;
         float fireAngle = Mathf.Atan2(fireDirection.y, fireDirection.x) * Mathf.Rad2Deg;
